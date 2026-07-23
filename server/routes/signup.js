163f2955383fb07382efe5e676249b5adcf82df4
takhdeer <tgrew803@mtroyal.ca>
2026-07-22 18:09:26 -0600

    feat:routed supabase auth to signup.jsx

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db')
+const { createClient } = require('@supabase/supabase-js')
+
+const supabaseAdmin = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY
+);
 
 
 router.post('/', async (req, res) => {
     console.log(req.body)
-    const { email, password} = req.body;
+    const { email, password, username} = req.body;
 
     if (!email || !password) {
         console.log('Missing Credentials')
@@ -13,23 +19,31 @@ router.post('/', async (req, res) => {
     }
 
     try {
-        const result = await pool.query(
-            'SELECT * FROM users WHERE user_email = $1', [email]
-        )
+         const {data, error} = await supabaseAdmin.auth.admin.createUser({
+            email,
+            password,
+            email_confirm: true
+         });
 
-        if (result.rows.length > 0) {
-            console.log('user exists')
-            return res.status(409).json({message: 'User exists'})
-        }
-        else{
-            await pool.query(
-                'INSERT INTO users (user_email, user_password) VALUES ($1,$2)', [email,password]
-            )
-            return res.status(201).json({message: "User created sucessfully!"})
-        }
+         if (error) {
+            if (error.status === 422 || error.code === 'email_exists') {
+                console.log('user exists');
+                return res.status(409).json({ message: 'User Exists'})
+            }
+            console.log(error);
+            return res.status(500).json({ message: 'Auth error', detail: error.message})
+         }
+
+         const authUserID = data.user.id
+
+        await pool.query(
+            'INSERT INTO users (id,user_email,user_password,username) VALUES ($1,$2,$3,$4)', [authUserID, email, password, username]
+        );
+
+        return res.status(201).json({ message: 'User created sucessfully'});
 
     }catch(err) {
-        console.log(err)
+        console.log(err.message)
         res.status(500).json({message: "Database error"})
     }
 });
