7dfe2ca3ecbfc0344765d0ce1e9227067b1c1abd
takhdeer <tgrew803@mtroyal.ca>
2026-08-22 15:50:58 -0600

    fix(engine): course returns undefined in getUserEmail()

--- server/engine/getUserEmails.js
+++ server/engine/getUserEmails.js
@@ -5,17 +5,24 @@ async function getUserEmails(courseInfo) {
     const courseId = courseInfo.courseId
     let emails = []
     try {
+
         const res = await pool.query(
+            'SELECT * FROM tracked_courses WHERE id = $1', [courseId]
+        );
+        const subject = res.rows[0].subject
+        const courseNum = res.rows[0].course_num
+        
+        const res2 = await pool.query(
             'SELECT users.id as user_id, users.user_email, user_courses.notified FROM user_courses JOIN users ON user_courses.user_id = users.id WHERE user_courses.course_id = $1',
             [courseId]
         );
 
-        console.log(`Found ${res.rows.length} users for course ${courseInfo.course}`);
+        console.log(`Found ${res2.rows.length} users for course ${courseInfo.course}`);
 
-        const result = res.rows.map(row => ({
+        const result = res2.rows.map(row => ({
             email: row.user_email,
             courseId: courseId,
-            course: courseInfo.course,
+            course: `${subject} ${courseNum}`,
             notified: row.notified
         }));
         emails.push(...result)
